d8a28d6db5816ae5a64bf14026705f8200985176
Tom Ruisseau <71035898+TomRuisseau@users.noreply.github.com>
2023-10-28 20:19:30 +0200

    Getting 32 games

--- app/src/main/java/com/example/tournament/MainActivity.java
+++ app/src/main/java/com/example/tournament/MainActivity.java
@@ -55,40 +55,33 @@ public class MainActivity extends AppCompatActivity {
         protected List<String> doInBackground(Void... params) {
             try {
                 String apiKey = "1c8158864e824bf181081be6d97b1504";
-                int totalItems = 64;
-                int maxPageSize = 40;
-                int totalPages = (int) Math.ceil((double) totalItems / maxPageSize);
+                int totalItems = 32;
 
                 List<String> games = new ArrayList<>();
 
-                for (int page = 1; page <= totalPages; page++) {
-                    int itemsNeeded = Math.min(totalItems - games.size(), maxPageSize); // Don't request more items than we need to
-                    String apiUrl = "https://api.rawg.io/api/games?ordering=popular&page=" + page + "&page_size=" + itemsNeeded + "&key=" + apiKey;
-                    System.out.println(apiUrl);
+                String apiUrl = "https://api.rawg.io/api/games?ordering=popular&page=1&page_size=" + totalItems + "&key=" + apiKey;
+                URL url = new URL(apiUrl);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
 
-                    URL url = new URL(apiUrl);
-                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                    connection.setRequestMethod("GET");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
+                StringBuilder response = new StringBuilder();
 
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                    String line;
-                    StringBuilder response = new StringBuilder();
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                reader.close();
 
-                    while ((line = reader.readLine()) != null) {
-                        response.append(line);
-                    }
-                    reader.close();
+                // Parse the JSON data and extract the names of the games for this page
+                JSONObject jsonResponse = new JSONObject(response.toString());
+                JSONArray resultsArray = jsonResponse.getJSONArray("results");
 
-                    // Parse the JSON data and extract the names of the games for this page
-                    JSONObject jsonResponse = new JSONObject(response.toString());
-                    JSONArray resultsArray = jsonResponse.getJSONArray("results");
-
-                    for (int i = 0; i < resultsArray.length(); i++) {
-                        JSONObject gameObject = resultsArray.getJSONObject(i);
-                        String gameName = gameObject.getString("name");
-                        String background_image = gameObject.getString("background_image");
-                        games.add("Game Name: " + gameName + "\nBackground Image: " + background_image);
-                    }
+                for (int i = 0; i < resultsArray.length(); i++) {
+                    JSONObject gameObject = resultsArray.getJSONObject(i);
+                    String gameName = gameObject.getString("name");
+                    String background_image = gameObject.getString("background_image");
+                    games.add("Game Name: " + gameName + "\nBackground Image: " + background_image);
                 }
 
                 return games;
