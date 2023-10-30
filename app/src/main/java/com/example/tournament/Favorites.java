74247cf0eb2a662246f4ca1800840e88ec89d5fc
TomRuisseau <tom.ruisseau.300302@gmail.com>
2023-10-30 19:34:50 +0100

    favorites display

--- app/src/main/java/com/example/tournament/Favorites.java
+++ app/src/main/java/com/example/tournament/Favorites.java
@@ -2,12 +2,16 @@ package com.example.tournament;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.tournament.dataClasses.Candidate;
+import com.example.tournament.executors.RetrieveFavoritesExecutor;
 import com.example.tournament.interfaces.FavoritesFetchedCallback;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -20,10 +24,9 @@ public class Favorites extends AppCompatActivity implements FavoritesFetchedCall
     TextView textViewShow, textViewMovie, textViewGame;
     ImageView imageViewShow, imageViewMovie, imageViewGame;
 
-    FirebaseFirestore db;
+    Button buttonChoicesFavorite;
 
-    FirebaseAuth mAuth;
-
+    RetrieveFavoritesExecutor retrieveFavoritesExecutor = new RetrieveFavoritesExecutor();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,20 +34,68 @@ public class Favorites extends AppCompatActivity implements FavoritesFetchedCall
         setContentView(R.layout.activity_favorites);
 
         //Initialize variables
-        mAuth = FirebaseAuth.getInstance();
-        db = FirebaseFirestore.getInstance();
         textViewShow = findViewById(R.id.textViewShow);
         textViewMovie = findViewById(R.id.textViewMovie);
         textViewGame = findViewById(R.id.textViewGame);
         imageViewShow = findViewById(R.id.imageViewShow);
         imageViewMovie = findViewById(R.id.imageViewMovie);
         imageViewGame = findViewById(R.id.imageViewGame);
+        buttonChoicesFavorite = findViewById(R.id.buttonChoicesFavorite);
+
+        buttonChoicesFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Menu.class);
+                startActivity(intent);
+            }
+        });
+
+
+        retrieveFavoritesExecutor.getFavorites(this::onFavoritesFetched);
     }
 
     @Override
     public void onFavoritesFetched(List<Candidate> favorites) {
-        for (Candidate candidate : favorites) {
-            Log.d("FAVORITES", candidate.getName());
+        Candidate game = favorites.get(0);
+        Candidate movie = favorites.get(1);
+        Candidate show = favorites.get(2);
+
+        int posterSizeX = 1800;
+        int posterSizeY = 2800;
+
+
+        if (game.getName() != null) {
+            textViewGame.setText(game.getName());
+            Picasso.get()
+                    .load(game.getImageUrl())
+                    .resize(1920, 1080)
+                    .centerCrop()
+                    .into(imageViewGame);
+        }
+        else{
+            textViewGame.setText(R.string.yet_to_be_determined);
+        }
+        if (movie.getName() != null) {
+            textViewMovie.setText(movie.getName());
+            Picasso.get()
+                    .load(movie.getImageUrl())
+                    .resize(posterSizeX, posterSizeY)
+                    .centerCrop()
+                    .into(imageViewMovie);
+        }
+        else{
+            textViewMovie.setText(R.string.yet_to_be_determined);
+        }
+        if (show.getName() != null) {
+            textViewShow.setText(show.getName());
+            Picasso.get()
+                    .load(show.getImageUrl())
+                    .resize(posterSizeX, posterSizeY)
+                    .centerCrop()
+                    .into(imageViewShow);
+        }
+        else{
+            textViewShow.setText(R.string.yet_to_be_determined);
         }
     }
 }
--- app/src/main/java/com/example/tournament/executors/RetrieveFavoritesExecutor.java
+++ app/src/main/java/com/example/tournament/executors/RetrieveFavoritesExecutor.java
@@ -2,17 +2,18 @@ package com.example.tournament.executors;
 
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 
 import com.example.tournament.dataClasses.Candidate;
 import com.example.tournament.interfaces.CandidatesFetchedCallback;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -22,18 +23,56 @@ public class RetrieveFavoritesExecutor {
     private ExecutorService mExecutorService;
     private Handler handler;
 
+    FirebaseFirestore db;
+
+    FirebaseAuth mAuth;
+    FirebaseUser currentUser;
+
+    List<Candidate> favorites = new ArrayList<>();
+
     public RetrieveFavoritesExecutor() {
         this.mExecutorService = Executors.newSingleThreadExecutor();
         this.handler = new Handler(Looper.getMainLooper());
+        this.mAuth = FirebaseAuth.getInstance();
+        this.currentUser = mAuth.getCurrentUser();
+        this.db = FirebaseFirestore.getInstance();
     }
 
     public  void getFavorites(CandidatesFetchedCallback callback){
         mExecutorService.execute(() -> {
             try {
+                // Get a reference to the users collection.
+                CollectionReference usersCollection = db.collection("users");
 
-                handler.post(() -> {
-                    callback.onCandidatesFetched(new ArrayList<>());
+                // Get a reference to the current user's document.
+                DocumentReference userDocumentReference = usersCollection.document(currentUser.getUid());
+
+                // Get a Task object for the query.
+                Task<DocumentSnapshot> userDocumentTask = userDocumentReference.get();
+
+                // Add a listener to the Task object that will be called when the task is complete and the document is successfully retrieved.
+                userDocumentTask.addOnSuccessListener(userDocumentSnapshot -> {
+                    // Get the data from the document snapshot.
+                    String gameName = userDocumentSnapshot.getString("Game_name");
+                    String gameImage = userDocumentSnapshot.getString("Game_image");
+                    String movieName = userDocumentSnapshot.getString("Movie_name");
+                    String movieImage = userDocumentSnapshot.getString("Movie_image");
+                    String showName = userDocumentSnapshot.getString("TV Show_name");
+                    String showImage = userDocumentSnapshot.getString("TV Show_image");
+
+                    Candidate game = new Candidate(gameName, gameImage, "game");
+                    Candidate movie = new Candidate(movieName, movieImage, "movie");
+                    Candidate show = new Candidate(showName, showImage, "show");
+
+                    favorites.add(game);
+                    favorites.add(movie);
+                    favorites.add(show);
+
+                    handler.post(() -> {
+                        callback.onCandidatesFetched(favorites);
+                    });
                 });
+
 
             } catch (Exception e) {
                 e.printStackTrace();
